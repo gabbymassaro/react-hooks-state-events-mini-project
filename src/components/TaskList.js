fe48210ab2b3dd07611da77acebd843447545090
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-13 10:21:32 -0400

    Render all tasks in taskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+
+  const displayTasks = tasks.map((task, index) => (
+    <Task key={index} text={task.text} />))
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayTasks}
     </div>
   );
 }
